7ddae290ff2a0457f345c1c42d435c8837419a9c
Nazar <rondelman15@gmail.com>
2022-11-02 18:32:25 +0200

    project init

---
+++ src/main/java/com/gameshop/gameshopapi/GameShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.gameshop.gameshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameShopApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gameshop/gameshopapi/GameShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameshop.gameshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
